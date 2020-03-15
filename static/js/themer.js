7c1fdda0bfbcc1bb0b5c0334c711cd4779a58da1
Russell Feldhausen <russfeldh@gmail.com>
2020-03-15 12:10:15 -0500

    Dark Theme

---
+++ static/js/themer.js
@@ -0,0 +1,27 @@
+//russfeld deal with themes
+var updateThemeLink = function(){
+  if($('body').hasClass('dark')){
+    $('#themelink').html('<i class="fas fa-sun"></i><span id="top-github-link-text">Light</span>');
+  }else{
+    $('#themelink').html('<i class="fas fa-moon"></i><span id="top-github-link-text">Dark</span>');
+  }
+}
+
+$(document).ready(function(){
+    var theme = localStorage.getItem('theme');
+    if(theme !== ''){
+        $('body').addClass(theme);
+    }
+    updateThemeLink();
+
+    $('#themelink').click(function () {
+      event.preventDefault();
+      $('body').toggleClass('dark');
+      if($('body').hasClass('dark')){
+          localStorage.setItem('theme', 'dark');
+      }else{
+          localStorage.removeItem('theme');
+      }
+      updateThemeLink();
+    });
+});
